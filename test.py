9a784005d0431013ef9f7adccaad7fd45ffd8478
lichengxiao1995 <lichengxiao1995@126.com>
2020-01-22 16:16:29 +0800

    实时画图

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ def open_file(filename):
 #绘制陀螺仪各欧拉角的角度、角速度的函数
 #para_x:csv第七列，表示imu时间（作为横坐标）
 #para_y:需要打印数据的列（作为纵坐标）
-def draw_fig(para_x,para_y):
+def draw_imu(para_x,para_y):
     g = 0
     for i in ['roll','pitch','yaw']:
         fig = plt.figure(i,dpi = 128,figsize = (12,6))
@@ -40,9 +40,19 @@ def draw_fig(para_x,para_y):
             k += 1
         para_y += 1
         plt.tight_layout()
+def scatter_imu(para_x,para_y):
+    x_axis_start = 550
+    x_axis_end = 551
+    for i in range(0,len(para_x)):
+        plt.scatter(para_x[i],para_y[i])
+        plt.pause(0.010)
 
+        if i % 10 == 0 and i > 1:
+            x_axis_start += 1
+            x_axis_end += 1
+            plt.axis([x_axis_start,x_axis_end,-10,10])
 #绘制：12个关节角度、角速度、力矩
-def draw_joint(para_x,para_y):
+def draw_joint(para_x, para_y):
     for r in ['Angle','velocity','Torque']:
         for i in ['HipX','HipY','Knee']:
             k = 1
@@ -66,9 +76,11 @@ filename,filetype = QFileDialog.getOpenFileName()
 
 #打开文件
 open_file(filename)
+#绘制点
+scatter_imu(para7,para8)
 #绘制陀螺仪信息，roll、pitch、yaw 三张图
-draw_fig(para7,8)
+#draw_imu(para7,8)
 #绘制12个关节信息
-draw_joint(para7,22)
+#draw_joint(para7,22)
 
 plt.show()
