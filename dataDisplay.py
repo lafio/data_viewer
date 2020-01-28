ec3a2b7ec1dc0e3436b2b46b993ef9da0e7f533e
lichengxiao1995 <lichengxiao1995@126.com>
2020-01-28 15:13:28 +0800

    添加了4个groupBox，并测试：将同一张图分别画在4个画板上，并将这4个画板放在上述4个groupBox上

---
+++ dataDisplay.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'dataDisplay.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(868, 813)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox.setGeometry(QtCore.QRect(21, 11, 779, 178))
+        self.groupBox.setObjectName("groupBox")
+        self.groupBox_2 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_2.setGeometry(QtCore.QRect(21, 195, 779, 178))
+        self.groupBox_2.setObjectName("groupBox_2")
+        self.groupBox_3 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_3.setGeometry(QtCore.QRect(21, 379, 779, 177))
+        self.groupBox_3.setObjectName("groupBox_3")
+        self.groupBox_4 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_4.setGeometry(QtCore.QRect(21, 562, 779, 178))
+        self.groupBox_4.setObjectName("groupBox_4")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 868, 23))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.groupBox.setTitle(_translate("MainWindow", "GroupBox"))
+        self.groupBox_2.setTitle(_translate("MainWindow", "GroupBox"))
+        self.groupBox_3.setTitle(_translate("MainWindow", "GroupBox"))
+        self.groupBox_4.setTitle(_translate("MainWindow", "GroupBox"))
--- start_dataDisplay.py
+++ start_dataDisplay.py
@@ -1,34 +1,37 @@
 from dataDisplay import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication,QMainWindow,QGridLayout
-from PyQt5.QtCore import QTimer
-import sys,time
-import numpy as np
-from matplotlib import pyplot as plt
+import sys
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.lines import Line2D
 
+#在mainwindow绘制曲线并添加到gridlayout
 class curve_Display(QMainWindow,Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.LineFigure = fig_Canvas()
-        self.LineFigure.drawit()
-        self.LineFigureLayout = QGridLayout(self.groupBox)
-        self.LineFigureLayout.addWidget(self.LineFigure)
-        #self.LineFigure.ax.set_xlim(-4, 4)
-        #self.LineFigure.ax.set_ylim(-1, 1)
-        #self.line = Line2D([-4,-3,-2,-1,1,2,3,4], [0.2,-0.3,0.9,-0.8,0.8,0.9,0,-0.4])
-        #self.LineFigure.ax.add_line(self.line)
+        for i in range(1,5):
+            self.gan(i)
+    def gan(self,n):
+        self.f = fig_Canvas()
+        self.f.drawit()
+        if n == 1:
+            self.f.Layout = QGridLayout(self.groupBox)
+        else:
+            self.f.Layout = QGridLayout(getattr(self,'groupBox_'+str(n)))
+        self.f.Layout.addWidget(self.f)
+
+#定义画板
 class fig_Canvas(FigureCanvas):
     def __init__(self):
         self.fig = Figure(figsize = (39,27),dpi=100)
+        self.fig.set_tight_layout(True)
         super().__init__(self.fig)
         self.ax = self.fig.add_subplot(111)
     def drawit(self):
         x = [i for i in range(9)]
         y = [i**2 for i in range(9)]
         self.ax.plot(x, y)
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ui = curve_Display()
